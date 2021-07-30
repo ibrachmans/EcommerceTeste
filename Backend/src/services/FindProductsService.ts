cb2ad595d4a5aa9bb5106d1c4de56658814b40a9
Ilan Brachmans <ilan.brachmans@gmail.com>
2021-07-30 18:50:18 -0300

    added products do database

--- Backend/src/services/FindProductsService.ts
+++ Backend/src/services/FindProductsService.ts
@@ -8,18 +8,8 @@ interface IProductRequest {
 class FindProductService {
   async execute({ name }: IProductRequest ) {
     const productsRepository = getCustomRepository(ProductsRepositories);
-
-    let productList = [];
-
-    if(name.length > 0) {
-      productList = await productsRepository.createQueryBuilder().where("product.name like :name", { name:`%${name}%` })
-      .getMany();
-      
-    } else {
-      productList = await productsRepository.find();
-    }
-
-    return productList;
+    
+    return await productsRepository.find();
   }
 }
 
