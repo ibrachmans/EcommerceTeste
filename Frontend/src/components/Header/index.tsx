a5b26142cc2779b3645c8fb39da38e5085e35630
Ilan Brachmans <ilan.brachmans@gmail.com>
2021-08-02 20:04:58 -0300

    added styles and search function

--- Frontend/src/components/Header/index.tsx
+++ Frontend/src/components/Header/index.tsx
@@ -1,15 +1,39 @@
-import { SearchBar } from '../SearchBar';
 import styles from './styles.module.scss';
+import React, { useState } from 'react';
+import { MdSearch, MdClear } from "react-icons/md";
+import { useRef } from 'react';
 
-export function Header() {
+export function Header({updateSearch}) {
+  const input = useRef<any>(null);
 
   return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        <h1>mmartan</h1>
-      
-        <SearchBar />
-      </div>
-    </header>
+      <header className={styles.headerContainer}>
+        <div className={styles.headerContent}>
+          <h1>Ecommerce</h1>
+          
+          <div className={styles.searchBarContainer}>
+            <span className={styles.searchIcon}><MdSearch /></span>
+            <input
+              ref={input}
+              className={styles.searchBar}
+              type="text"
+              placeholder="Search..."
+              onChange={(input) => 
+                updateSearch(input.currentTarget.value)
+              }
+
+            />
+            <button
+              className={styles.searchClear}
+              onClick={() => {
+                updateSearch("");
+                input.current.value = ""
+              }}
+            >
+             <MdClear />
+            </button>
+          </div>
+        </div>
+      </header>
   )
 }
--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import styles from './home.module.scss';
 import { api } from '../services/api';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { Header } from '../components/Header';
 
 interface Props {
   products: Product[]
@@ -19,9 +20,14 @@ interface Product {
 }
 
 export default function Home(props: Props) {
-  const [searchTitle, setSearchTitle] = useState("Lista de Produtos");
+  const [searchTitle, setSearchTitle] = useState("Lista de produtos");
+
   const updateSearch = (text) => {
-    setSearchTitle(text);
+    if (text.length == 0) {
+      setSearchTitle("Lista de produtos");
+    } else {
+      setSearchTitle(text);
+    }
   }
 
   return (
@@ -29,11 +35,10 @@ export default function Home(props: Props) {
       <Head>
         <title>Desafio Ecommerce</title>
       </Head>
-      
+      <Header updateSearch={(text) => updateSearch(text)} />
       <div className={styles.searchTitleContainer}>
         <h1 className={styles.searchTitleContent}>{searchTitle}</h1>
       </div>
-      
       <main className={styles.mainContainer}>
         <span className={styles.productFound}>{`${props.products.length} PRODUTO(S) ENCONTRADO(S)`}</span>
         <span className={styles.underline}></span>
@@ -44,8 +49,10 @@ export default function Home(props: Props) {
               <h3>{product.name}</h3>
               <p>{product.category}</p>
             </div>
-            <div>
-              {`R$${product.price} ${product.discount}`}
+            <div className={styles.productPrice}>
+              <span className={styles.originalPrice}>{`R$${product.price}`}</span>
+              
+              <span className={styles.salePrice}>{`por ${product.discount}`}</span>
             </div>
           </section>
         ))}
